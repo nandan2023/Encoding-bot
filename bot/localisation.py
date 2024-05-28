5264f234cc05dc39f82228ea23daa101509fe684
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-05-28 07:01:57 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -13,31 +13,31 @@ class Localisation:
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     
     
-    DOWNLOAD_START = "Downloading ... 📥 \n"
+    DOWNLOAD_START = "⚡ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ.... \n"
     
-    UPLOAD_START = "Uploading ... 📤 \n"
+    UPLOAD_START = "⚡ᴜᴘʟᴏᴀᴅɪɴɢ.... \n"
     
-    COMPRESS_START = "ᴛʀʏɪɴɢ ᴛᴏ ᴇɴᴄᴏᴅᴇ ... 📀"
+    COMPRESS_START = "⚡ᴛʀʏɪɴɢ ᴛᴏ ᴇɴᴄᴏᴅᴇ..."
     
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "ᴇɴᴄᴏᴅᴇᴅ ʙʏ [@Anime_Sensei_Network]"
+    COMPRESS_SUCCESS = "ᴇɴᴄᴏᴅᴇᴅ ʙʏ [@Ongoing_Sensei]"
 
-    COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
+    COMPRESS_PROGRESS = "🕛 ᴇᴛᴀ {}\n♻️ ᴘʀᴏɢʀᴇss {}%"
 
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully...✅"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "ᴛʜᴜᴍʙɴᴀɪʟ ᴄʟᴇᴀʀᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ...✅"
     
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully... ✅"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "ᴍᴇᴅɪᴀ ᴄʟᴇᴀʀᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ... ✅"
     
-    SAVED_RECVD_DOC_FILE = "Downloaded Successfully...✅"
+    SAVED_RECVD_DOC_FILE = "ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ...✅"
     
     CUSTOM_CAPTION_UL_FILE = " "
     
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found...💔"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "ɴᴏ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ғᴏᴜɴᴅ...❌"
     
     NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
     
