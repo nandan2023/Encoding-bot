d68e73b031b3f3dfc2dd62aa796c188e4fe8eb3b
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-05-28 07:28:00 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Config(object):
      # Get these values from my.telegram.org
     AUTH_USERS = set(
         int(x) for x in get_config(
-            "2036803347 6691641006 -1002103913042",
+            "2036803347" "6691641006" "-1002103913042",
             should_prompt=True
         ).split()
     )
