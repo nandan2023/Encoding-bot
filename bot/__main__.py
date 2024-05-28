96928891dea4c1e7b9f22a306bb4283ebdbc2cc4
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-05-28 06:42:06 +0530

    Add files via upload

---
+++ bot/__main__.py
@@ -0,0 +1,242 @@
+# oof
+from datetime import datetime as dt
+import os, asyncio, pyrogram, psutil, platform
+from bot import (
+    APP_ID,
+    API_HASH,
+    AUTH_USERS,
+    DOWNLOAD_LOCATION,
+    LOGGER,
+    TG_BOT_TOKEN,
+    BOT_USERNAME,
+    SESSION_NAME,
+    
+    data,
+    app,
+    crf,
+    resolution,
+    audio_b,
+    preset,
+    codec,
+    watermark 
+)
+from bot.helper_funcs.utils import add_task, on_task_complete, sysinfo
+from pyrogram import Client, filters, enums
+from pyrogram.handlers import MessageHandler, CallbackQueryHandler
+from pyrogram.types import Message
+from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
+
+from bot.plugins.incoming_message_fn import (
+    incoming_start_message_f,
+    incoming_compress_message_f,
+    incoming_cancel_message_f
+)
+
+
+from bot.plugins.status_message_fn import (
+    eval_message_f,
+    exec_message_f,
+    upload_log_file
+)
+
+from bot.commands import Command
+from bot.plugins.call_back_button_handler import button
+sudo_users = "2036803347" 
+crf.append("23")
+codec.append("libx264")
+resolution.append("854x480 ")
+preset.append("veryfast")
+audio_b.append("40k")
+# 🤣
+
+
+uptime = dt.now()
+
+def ts(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+        + ((str(milliseconds) + "ms, ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+if __name__ == "__main__" :
+    # create download directory, if not exist
+    if not os.path.isdir(DOWNLOAD_LOCATION):
+        os.makedirs(DOWNLOAD_LOCATION)
+    #
+    
+    
+    #
+    app.set_parse_mode(enums.ParseMode.MARKDOWN)
+    #
+    # STATUS ADMIN Command
+
+    # START command
+    incoming_start_message_handler = MessageHandler(
+        incoming_start_message_f,
+        filters=filters.command(["start", f"start@{BOT_USERNAME}"])
+    )
+    app.add_handler(incoming_start_message_handler)
+    
+    
+    @app.on_message(filters.incoming & filters.command(["crf", f"crf@{BOT_USERNAME}"]))
+    async def changecrf(app, message):
+        if message.from_user.id in AUTH_USERS:
+            cr = message.text.split(" ", maxsplit=1)[1]
+            OUT = f"I will be using : {cr} crf"
+            crf.insert(0, f"{cr}")
+            await message.reply_text(OUT)
+        else:
+            await message.reply_text("Admin Only 🔒")
+            
+    @app.on_message(filters.incoming & filters.command(["settings", f"settings@{BOT_USERNAME}"]))
+    async def settings(app, message):
+        if message.from_user.id in AUTH_USERS:
+            await message.reply_text(f"The current settings will be added to your video file :\n\nCodec : {codec[0]} \nCrf : {crf[0]} \nResolution : {resolution[0]} \nPreset : {preset[0]} \nAudio Bitrates : {audio_b[0]}")
+        else:
+            await message.reply_text("Admin Only 🔒")
+
+    @app.on_message(filters.incoming & filters.command(["sysinfo", f"sysinfo@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+       if message.from_user.id in AUTH_USERS:
+           await sysinfo(message)
+       else:
+           await message.reply_text("Admin Only 🔒")
+               
+    @app.on_message(filters.incoming & filters.command(["resolution", f"resolution@{BOT_USERNAME}"]))
+    async def changer(app, message):
+        if message.from_user.id in AUTH_USERS:
+            r = message.text.split(" ", maxsplit=1)[1]
+            OUT = f"I will be using : {r} resolution"
+            resolution.insert(0, f"{r}")
+            await message.reply_text(OUT)
+        else:
+            await message.reply_text("Admin Only 🔒")
+
+            
+               
+    @app.on_message(filters.incoming & filters.command(["preset", f"preset@{BOT_USERNAME}"]))
+    async def changepr(app, message):
+        if message.from_user.id in AUTH_USERS:
+            pop = message.text.split(" ", maxsplit=1)[1]
+            OUT = f"I will be using : {pop} preset"
+            preset.insert(0, f"{pop}")
+            await message.reply_text(OUT)
+        else:
+            await message.reply_text("Admin Only 🔒")
+
+            
+    @app.on_message(filters.incoming & filters.command(["codec", f"codec@{BOT_USERNAME}"]))
+    async def changecode(app, message):
+        if message.from_user.id in AUTH_USERS:
+            col = message.text.split(" ", maxsplit=1)[1]
+            OUT = f"I will be using : {col} codec"
+            codec.insert(0, f"{col}")
+            await message.reply_text(OUT)
+        else:
+            await message.reply_text("Admin Only 🔒")
+             
+    @app.on_message(filters.incoming & filters.command(["audio", f"audio@{BOT_USERNAME}"]))
+    async def changea(app, message):
+        if message.from_user.id in AUTH_USERS:
+            aud = message.text.split(" ", maxsplit=1)[1]
+            OUT = f"I will be using : {aud} audio"
+            audio_b.insert(0, f"{aud}")
+            await message.reply_text(OUT)
+        else:
+            await message.reply_text("Admin Only 🔒")
+            
+        
+    @app.on_message(filters.incoming & filters.command(["compress", f"compress@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        if message.chat.id not in AUTH_USERS:
+            return await message.reply_text("You are not authorised to use this bot contact @Sensei_Rimuru")
+        query = await message.reply_text("Added to Queue ⏰...\nPlease be patient, Compress will start soon", quote=True)
+        data.append(message.reply_to_message)
+        if len(data) == 1:
+         await query.delete()   
+         await add_task(message.reply_to_message)     
+ 
+    @app.on_message(filters.incoming & filters.command(["restart", f"restart@{BOT_USERNAME}"]))
+    async def restarter(app, message):
+        if message.from_user.id in AUTH_USERS:
+            await message.reply_text("Restarting... ♻️")
+            quit(1)
+        else:
+            await message.reply_text("Admin Only 🔒")
+            
+    @app.on_message(filters.incoming & filters.command(["clear", f"clear@{BOT_USERNAME}"]))
+    async def restarter(app, message):
+        if message.from_user.id in AUTH_USERS:
+            await message.reply_text("Successfully cleared Queue ...📚")
+        else:
+            await message.reply_text("Admin Only 🔒")
+         
+        
+    @app.on_message(filters.incoming & (filters.video | filters.document))
+    async def help_message(app, message):
+        if message.chat.id not in AUTH_USERS:
+            return await message.reply_text("You are not authorised to use this bot contact @Sensei_Rimuru")
+        query = await message.reply_text("Added to Queue ⏰...\nPlease be patient, Compress will start soon", quote=True)
+        data.append(message)
+        if len(data) == 1:
+         await query.delete()   
+         await add_task(message)
+            
+    @app.on_message(filters.incoming & (filters.photo))
+    async def help_message(app, message):
+        if message.chat.id not in AUTH_USERS:
+            return await message.reply_text("You are not authorised to use this bot contact @Sensei_Rimuru")
+        os.system('rm thumb.jpg')
+        await message.download(file_name='/app/thumb.jpg')
+        await message.reply_text('Thumbnail Added 🎴')
+        
+    @app.on_message(filters.incoming & filters.command(["cancel", f"cancel@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await incoming_cancel_message_f(app, message)
+        
+        
+    @app.on_message(filters.incoming & filters.command(["exec", f"exec@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await exec_message_f(app, message)
+        
+    @app.on_message(filters.incoming & filters.command(["eval", f"eval@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await eval_message_f(app, message)
+        
+    @app.on_message(filters.incoming & filters.command(["stop", f"stop@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await on_task_complete()    
+   
+    @app.on_message(filters.incoming & filters.command(["help", f"help@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await message.reply_text("Hi, I am  Video Encoder bot. \n\n➥ Send me your telegram files\n➥ I will encode them one by one. ", quote=True)
+  
+    @app.on_message(filters.incoming & filters.command(["log", f"log@{BOT_USERNAME}"]))
+    async def help_message(app, message):
+        await upload_log_file(app, message)
+    @app.on_message(filters.incoming & filters.command(["ping", f"ping@{BOT_USERNAME}"]))
+    async def up(app, message):
+      stt = dt.now()
+      ed = dt.now()
+      v = ts(int((ed - uptime).seconds) * 1000)
+      u = f"Bot Uptime = {v} 🚀"
+      ms = (ed - stt).microseconds / 1000
+      p = f"Pɪɴɢ = {ms}ms 🌋"
+      await message.reply_text(u + "\n" + p)
+
+    call_back_button_handler = CallbackQueryHandler(
+        button
+    )
+    app.add_handler(call_back_button_handler)
+
+    # run the APPlication
+    app.run()
---
+++ bot/localisation.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AbirHasan2005
+
+from bot.get_cfg import get_config
+
+
+class Localisation:
+    START_TEXT = "Hello, This is a Telegram Video Encoder Bot. \n\n Please send me any Telegram big video file I will compress it as  small video file! \n\n/help for more details. \n\nOwner : @Sensei_Rimuru"
+   
+    ABS_TEXT = " Please don't be selfish."
+    
+    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
+    
+    
+    DOWNLOAD_START = "Downloading ... 📥 \n"
+    
+    UPLOAD_START = "Uploading ... 📤 \n"
+    
+    COMPRESS_START = "ᴛʀʏɪɴɢ ᴛᴏ ᴇɴᴄᴏᴅᴇ ... 📀"
+    
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    
+    COMPRESS_SUCCESS = "ᴇɴᴄᴏᴅᴇᴅ ʙʏ [@Anime_Sensei_Network]"
+
+    COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+    
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully...✅"
+    
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully... ✅"
+    
+    SAVED_RECVD_DOC_FILE = "Downloaded Successfully...✅"
+    
+    CUSTOM_CAPTION_UL_FILE = " "
+    
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found...💔"
+    
+    NO_VOID_FORMAT_FOUND = "no-one gonna help you\n{}"
+    
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already one Process going on! ⚠️ \n\nCheck Live Status on Anime Sensei Logs."
+    
+    HELP_MESSAGE = get_config(
+        "STRINGS_HELP_MESSAGE",
+        "Hi, I am Video Compressor Bot."
+    )
+    WRONG_MESSAGE = get_config(
+        "STRINGS_WRONG_MESSAGE",
+        "current CHAT ID: <code>{CHAT_ID}</code>"
+    )
+
+    
