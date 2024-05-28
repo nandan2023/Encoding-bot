c97e0fa5a9da1f32dc901627d1b6705aaa9575ed
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-05-28 08:55:17 +0530

    Update display_progress.py

--- bot/helper_funcs/display_progress.py
+++ bot/helper_funcs/display_progress.py
@@ -49,12 +49,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n♻️*ᴘʀᴏᴄᴇssɪɴɢ*:</b> {2}%\n".format(
+        progress = "[{0}{1}] \n♻️**ᴘʀᴏᴄᴇssɪɴɢ**: {2}%\n".format(
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n*sᴘᴇᴇᴅ*: {2}/s\n*ᴇᴛᴀ*: {3}\n".format(
+        tmp = progress + "{0} of {1}\n**sᴘᴇᴇᴅ**: {2}/s\n**ᴇᴛᴀ**: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
