78facbee006015bb952b328b78dd24d441eadfa3
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-05-28 08:54:30 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -140,13 +140,13 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
         if difference > 0:
           ETA = TimeFormatter(difference*1000)
         percentage = math.floor(elapsed_time * 100 / total_time)
-        progress_str = "♻️ *ᴘʀᴏɢʀᴇss*: {0}%\n[{1}{2}]".format(
+        progress_str = "♻️ **ᴘʀᴏɢʀᴇss**: {0}%\n[{1}{2}]".format(
             round(percentage, 2),
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))])
             )
-        stats = f'⚡ *ᴇɴᴄᴏᴅɪɴɢ ɪɴ ᴘʀᴏɢʀᴇss*\n\n' \
-                f'🕛 *ᴛɪᴍᴇ ʟᴇғᴛ*: {ETA}\n\n' \
+        stats = f'⚡ **ᴇɴᴄᴏᴅɪɴɢ ɪɴ ᴘʀᴏɢʀᴇss**\n\n' \
+                f'🕛 **ᴛɪᴍᴇ ʟᴇғᴛ**: {ETA}\n\n' \
                 f'{progress_str}\n'
         try:
           await message.edit_text(
