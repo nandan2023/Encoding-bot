c732c11146d57362ff78db37f5fc36b0fa27fc07
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2024-05-30 18:41:12 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,7 +44,7 @@ from bot.plugins.call_back_button_handler import button
 sudo_users = "2036803347" 
 crf.append("25")
 codec.append("libx264")
-resolution.append("1920x1080")
+resolution.append("1280x720")
 preset.append("veryfast")
 audio_b.append("40k")
 # 🤣
